875a65831256958f7dafb4732f4c51496fa3e000
Fasasi Qasim <fasaqa1000@gmail.com>
2023-05-03 13:23:41 +0100

    added a new migration for user

--- Rellish/Data/ApplicationDbContext.cs
+++ Rellish/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Rellish.Models;
 
 namespace Rellish.Data
 {
-    public class ApplicationDbContext :IdentityDbContext
+    public class ApplicationDbContext :IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
+
+ 
 }
--- Rellish/Program.cs
+++ Rellish/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Rellish.Data;
+using Rellish.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

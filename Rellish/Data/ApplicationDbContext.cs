8ae6919a3dee2ba51346dc28adf3a90387c52071
Fasasi Qasim <fasaqa1000@gmail.com>
2023-05-03 13:35:05 +0100

    added menuitem and migration

--- Rellish/Data/ApplicationDbContext.cs
+++ Rellish/Data/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace Rellish.Data
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
     }
 
  

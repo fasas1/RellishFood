3b15296aff8de41118702210d9c953c16a6ac731
Fasasi Qasim <fasaqa1000@gmail.com>
2023-05-03 15:04:08 +0100

    Added a GET request for all menu Items

---
+++ Rellish/Controllers/MenuItemController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rellish.Data;
+using Rellish.Models;
+using System.Net;
+
+namespace Rellish.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public MenuItemController(ApplicationDbContext  db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+             return Ok(_response);
+        }
+    }
+}

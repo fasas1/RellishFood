2b8abbeb7a4dc57fd8ee9c32a8c9a0e6d2980384
Fasasi Qasim <fasaqa1000@gmail.com>
2023-05-13 04:23:13 +0100

    added a shopping cart

--- Rellish/Controllers/ShoppingCartController.cs
+++ Rellish/Controllers/ShoppingCartController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rellish.Data;
+using Rellish.Models;
+using System.Net;
 
 namespace Rellish.Controllers
 {
@@ -7,5 +11,79 @@ namespace Rellish.Controllers
     [ApiController]
     public class ShoppingCartController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _response = new();
+            _db = db;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u => u.CartItems).FirstOrDefault(u => u.UserId == userId);
+            MenuItem    menuItem  = _db.MenuItems.FirstOrDefault(u => u.Id ==  menuItemId);
+            if(menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            if (shoppingCart == null && updateQuantityBy > 0)
+            {
+                ShoppingCart newCart = new() { UserId = userId };
+                _db.ShoppingCarts.Add(newCart);
+                _db.SaveChanges();
+
+                CartItem newCartItem = new()
+                {
+                    MenuItemId = menuItemId,
+                    Quantity = updateQuantityBy,
+                    ShoppingCartId = newCart.Id,
+                    MenuItem = null
+                };
+                _db.CartItems.Add(newCartItem);
+                _db.SaveChanges();
+            }
+            else
+            {
+                CartItem cartItemInCart = shoppingCart.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+                if(cartItemInCart == null)
+                {
+                    CartItem newCartItem = new()
+                    {
+                        MenuItemId = menuItemId,
+                        Quantity = updateQuantityBy,
+                        ShoppingCartId = shoppingCart.Id,
+                        MenuItem = null
+                    };
+                    _db.CartItems.Add(newCartItem);
+                    _db.SaveChanges();
+                }
+                else
+                {
+                  int newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+                   if(updateQuantityBy == 0 || newQuantity <= 0)
+                    {
+                        _db.CartItems.Remove(cartItemInCart);
+                        if(shoppingCart.CartItems.Count() == 0)
+                        {
+                            _db.CartItems.Remove(cartItemInCart);
+                        }
+                        _db.SaveChanges();
+                    }
+                    else
+                    {
+                        cartItemInCart.Quantity = newQuantity;
+                        _db.SaveChanges();
+                    }
+                }
+
+            }
+            return _response;
+        }
     }
 }

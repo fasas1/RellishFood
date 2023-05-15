a9324fbf6bacd2685648337989b6469cc6439dd4
Fasasi Qasim <fasaqa1000@gmail.com>
2023-05-15 12:28:14 +0100

    added migration for the orderheader and details

--- Rellish/Data/ApplicationDbContext.cs
+++ Rellish/Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace Rellish.Data
         public DbSet<ShoppingCart>  ShoppingCarts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
         public DbSet<OrderHeader> OrderHeaders { get; set; }
-        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Rellish/Models/OrderHeader.cs
+++ Rellish/Models/OrderHeader.cs
@@ -1,6 +1,30 @@
-﻿namespace Rellish.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rellish.Models
 {
     public class OrderHeader
     {
+        [Key]
+        public int OrderHeaderId { get; set; }
+        [Required]
+        public string PickUpName { get; set; }
+        [Required]
+        public string PickUpPhoneNumber { get; set; }
+        [Required]
+        public string PickUpEmail { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser User { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public DateTime OrderDate { get; set; }
+        public int StripePaymentIntentId { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
     }
 }

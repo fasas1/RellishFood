3080932552087ca58280f278dbce59c2345bc73c
Fasasi Qasim <fasaqa1000@gmail.com>
2023-05-22 01:33:02 +0100

    fixes

--- Rellish/Controllers/ShoppingCartController.cs
+++ Rellish/Controllers/ShoppingCartController.cs
@@ -25,16 +25,19 @@ namespace Rellish.Controllers
         {
             try
             {
-              if(string.IsNullOrEmpty(userId))
+                ShoppingCart shoppingCart;
+                if (string.IsNullOrEmpty(userId))
                 {
-                    _response.IsSuccess = false;
-                    _response.StatusCode = HttpStatusCode.BadRequest;
-                    return BadRequest(_response);
+                    shoppingCart = new();
                 }
-                ShoppingCart shoppingCart= _db.ShoppingCarts
-                    .Include(u => u.CartItems)
-                    .ThenInclude(u => u.MenuItem).FirstOrDefault(u => u.UserId == userId);
-                if(shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0)
+                else
+                {
+                    shoppingCart = _db.ShoppingCarts
+                    .Include(u => u.CartItems).ThenInclude(u => u.MenuItem)
+                    .FirstOrDefault(u => u.UserId == userId);
+
+                }
+                if (shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0)
                 {
                     shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
                 }
